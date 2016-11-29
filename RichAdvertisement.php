573a46241c115f8490ab19f741321ee683f1f9e7
sunnysideup <sunnysideupdevs@gmail.com>
2016-11-29 14:03:55 +1300

    PATCH: module clean-up

--- RichAdvertisement.php
+++ RichAdvertisement.php
@@ -2,42 +2,42 @@
 /**
  *@author nicolaas [at] sunnysideup.co.nz
  **/
-class RichAdvertisement extends Widget {
+class RichAdvertisement extends Widget
+{
+    public static $has_one = array(
+        "RichAdvertisementWidgetObject" => "RichAdvertisementWidgetObject"
+    );
 
-	static $has_one = array(
-		"RichAdvertisementWidgetObject" => "RichAdvertisementWidgetObject"
-	);
+    public static $title = 'Rich Advertisement';
 
-	static $title = 'Rich Advertisement';
+    public static $cmsTitle = 'Rich Advertisement';
 
-	static $cmsTitle = 'Rich Advertisement';
+    public static $description = 'Allows you to add a richly formatted advertisement';
 
-	static $description = 'Allows you to add a richly formatted advertisement';
+    public function getCMSFields()
+    {
+        $options = DataObject::get("RichAdvertisementWidgetObject");
+        if ($options) {
+            return new FieldSet(
+                new LiteralField("RichAdvertisementWidgetObjectLink", "Link to modelAdmin goes here..."),
+                new DropdownField(
+                    $name = "RichAdvertisementWidgetObjectID",
+                    $title = "Select advertisement",
+                    $source = $options
+                )
+            );
+        } else {
+            //put some message here on how to add options
+        }
+    }
 
-	function getCMSFields() {
-		$options = DataObject::get("RichAdvertisementWidgetObject");
-		if($options) {
-			return new FieldSet(
-				new LiteralField("RichAdvertisementWidgetObjectLink", "Link to modelAdmin goes here..."),
-				new DropdownField(
-					$name = "RichAdvertisementWidgetObjectID",
-					$title = "Select advertisement",
-					$source = $options
-				)
-			);
-		}
-		else {
-			//put some message here on how to add options
-		}
-	}
+    public function Title()
+    {
+        return $this->WidgetTitle ? $this->WidgetTitle : self::$title;
+    }
 
-	function Title() {
-		return $this->WidgetTitle ? $this->WidgetTitle : self::$title;
-	}
-
-	function getTitle() {
-		return $this->Title;
-	}
-
-
+    public function getTitle()
+    {
+        return $this->Title;
+    }
 }
--- _config.php
+++ _config.php
@@ -8,7 +8,3 @@
 //copy the lines between the START AND END line to your /mysite/_config.php file and choose the right settings
 //===================---------------- START widgets_richadvertisement MODULE ----------------===================
 //===================---------------- END widgets_richadvertisement MODULE ----------------===================
-
-
-
-
